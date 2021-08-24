927bb6c9567c1d2d574e1ce226552585ef82f05e
sean <gmlssns5859@gmail.com>
2021-08-24 17:09:40 +0900

    test

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import numpy as np
-t = np.array([1, 5, 7, 33, 39, 52])
+t = np.array([1, 5, 7, 33, 39, 52]
+            [2, 3, 4, 1, 2, 4]
+            [3, 4, 2, 10, 4, 3])
 
 # test enumerate
 # for i in enumerate(t):
 #     print(i)
 
 # test argmax
-# print(t)
+print(t)
 # print(t.argmax(axis=0))
 
